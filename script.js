5ba04898e170b296394c75ad126df29ff9014ed7
tod619 <trevorodwyer619@hotmail.com>
2021-09-28 20:16:57 +0100

    get ball to interact with paddle in script.js file

--- script.js
+++ script.js
@@ -117,6 +117,11 @@ function moveBall() {
     if(ball.y + ball.size > canvas.height || ball.y - ball.size < 0){
         ball.dy *= -1
     }
+
+    // Paddle Collision
+    if(ball.x - ball.size > paddle.x && ball.x + ball.size < paddle.x + paddle.w && ball.y + ball.size > paddle.y) {
+        ball.dy = -ball.speed
+    }
 }
 
 // Draw Everything
